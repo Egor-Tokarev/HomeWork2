2d69a6a5d468fcd71f0a66d1c50d12a381fd1ef8
Егор Токорев <tokarev9297@yandex.ru>
2022-07-29 19:57:20 +0300

    1func

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Car {
+    int power;
+    String auto;
+
+    public Car (int power, String auto) {
+        this.power = power;
+        this.auto = auto;
+    }
+
+    void toRace() {
+        if (power > 600)
+            System.out.println("Могу учавствовать в Топ32 ");
+        else {
+            System.out.println("Недостаточно мощности, сынок");
+        }
+    }
+}
